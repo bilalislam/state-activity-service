bfb622b5b1b5a451ebd92943d579251d5e7fd2a3
bilal.islam <bilal.islam@turk.net>
2025-03-14 02:39:29 +0300

    refactored II

--- src/test/java/com/turknet/stateactivityservice/common/ContainersConfig.java
+++ src/test/java/com/turknet/stateactivityservice/common/ContainersConfig.java
@@ -1,18 +1,16 @@
 package com.turknet.stateactivityservice.common;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfig {
 
     @Bean
-    @ServiceConnection
     public MongoDBContainer mongoDBContainer() {
-
-        return new MongoDBContainer(DockerImageName.parse("mongo:4.4.6"));
+        MongoDBContainer container = new MongoDBContainer("mongo:4.4.6");
+        container.start();
+        return container;
     }
 }
